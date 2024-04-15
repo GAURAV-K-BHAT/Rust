809ce1e20d4707fae7b08313ef3ca33f405228c5
Gaurav Bhat <gauravbhat2008@gmail.com>
2024-04-16 02:31:55 +0530

    added_variables

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let secrate_number = rand::thread_rng().gen_range(1..=100);
     loop {
             // println!("The secrate number is {secrate_number}");
-            println!( "{}","Please Enter the number ...".trim().on_white().bold());
+            println!( "{}","Please Enter the number ...".trim().on_white().bold().truecolor(0,0,0));
 
             let mut guess = String::new();
 
@@ -24,10 +24,10 @@ fn main() {
                 println!(" Your Guessed Number Is : {guess}");
 
                 match guess.cmp(&secrate_number){
-                    Ordering::Less => println!("{}","Input Is Low ".on_yellow()),
-                    Ordering::Greater => println! ("{}","Input Is High".on_red()),
+                    Ordering::Less => println!("{}","Input Is Low ".on_yellow().truecolor(0,0,0)),
+                    Ordering::Greater => println! ("{}","Input Is High".on_red().truecolor(0,0,0)),
                     Ordering::Equal => {
-                        println!("{}","Congratulations You Won !!! " .on_green().green().italic().bold()); 
+                        println!("{}","Congratulations You Won !!! " .on_green().green().italic().bold().truecolor(0,0,0)); 
                         break;
                     }
 
